d917ee60b47ec9c155a62b83b3ee7c266dc1dd86
yonzilch <git@yonzilch.com>
2026-07-05 03:22:06 +0800

    chore(nix): add flake-based development shell
    Add a Nix flake and direnv entrypoint for the Arata development environment.
    
    This introduces a reproducible local shell for working on the project with the required toolchain available through the flake. The lock file is committed to keep the development inputs stable across machines.
    
    Included changes:
    - Add flake.nix for the development shell
    - Add flake.lock to pin flake inputs
    - Add .envrc for direnv-based shell activation
    - Ignore local Nix or direnv artifacts where applicable
    - Tune .gitignore file

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "Bun and Gleam development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    devshell.url = "github:numtide/devshell";
+    devshell.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      devshell,
+    }:
+    flake-utils.lib.eachSystem [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ] (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ devshell.overlays.default ];
+        };
+      in
+      {
+        devShells.default = pkgs.devshell.mkShell {
+          packages = with pkgs; [
+            bun
+            gleam
+          ];
+        };
+      }
+    );
+}
