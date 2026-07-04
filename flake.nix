e46b5bbf4ae8afd39b7d3f43ef863793bfb08edf
yonzilch <git@yonzilch.com>
2026-07-05 04:25:27 +0800

    fix(nix): accept implicit flake outputs arguments
    Allow the flake outputs function to receive implicit arguments such as self without binding unused names.
    
    This fixes nix develop failing with an unexpected self argument while avoiding an unused self binding reported by nixf.
    
    The outputs argument set now uses an ellipsis to accept extra flake-provided inputs safely.

--- flake.nix
+++ flake.nix
@@ -10,10 +10,10 @@
 
   outputs =
     {
-      self,
       nixpkgs,
       flake-utils,
       devshell,
+      ...
     }:
     flake-utils.lib.eachSystem [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ] (
       system:
